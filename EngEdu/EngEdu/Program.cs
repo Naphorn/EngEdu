ac4757157c0d7b418cdab9c79a07de4988a89886
Naphorn <naphorn.chok@gmail.com>
2017-10-14 21:15:51 +0700

    3rd year semester 2

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -13,6 +13,7 @@ namespace EngEdu
             Console.WriteLine("Year 255x semester 1, I am an Engedu freshman.");
             Console.WriteLine("Year 255x semester 2, I learn to program in C sharp.");
             Console.WriteLine("Year 2560 semester 1, I have to regrade on some course.");
+            Console.WriteLine("Year 2560 semester 2, I learn to web in Java.");
         }
     }
 }
